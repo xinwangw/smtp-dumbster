8f2105182d38894a325b7f949b7afb0dca41de05
Xin Wang <xinwangw@gmail.com>
2017-04-01 22:52:46 +1100

    remove save email as HTML

--- src/main/java/local/smtp/ScheduledTasks.java
+++ src/main/java/local/smtp/ScheduledTasks.java
@@ -1,10 +1,6 @@
 package local.smtp;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.sql.Blob;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -12,9 +8,7 @@ import java.util.Iterator;
 
 import javax.mail.internet.MimeUtility;
 
-import org.apache.commons.io.FileUtils;
 import org.hibernate.Hibernate;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,7 +20,6 @@ import com.dumbster.smtp.SimpleSmtpServer;
 import com.dumbster.smtp.SmtpMessage;
 
 import local.smtp.model.Inbox;
-import local.smtp.repository.InboxRepository;
 import local.smtp.service.InboxService;
 
 @Component
@@ -35,7 +28,6 @@ public class ScheduledTasks {
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-    private static final String MAIN_PATH = "./target/inbox/";
 
     @Autowired
     private SimpleSmtpServer server;
@@ -65,10 +57,7 @@ public class ScheduledTasks {
         	log.info("Subject: "+subject);
         	log.info(email.getBody());
         	try {
-        		FileUtils.forceMkdir(new File(MAIN_PATH));
-        		File file = FileUtils.getFile(MAIN_PATH + URLEncoder.encode(subject+"-"+current.getTime())+".html");
-				FileUtils.write(file, wrapEmailTo(email.getHeaderValue("To"))+wrapEmailFrom(email.getHeaderValue("From"))+wrapEmailDate(current)+wrapSubject(subject)+email.getBody(), "UTF8");
-				Inbox inboxData = new Inbox();
+        		Inbox inboxData = new Inbox();
 				inboxData.setCreateDate(current);
 				inboxData.setSubject(subject);
 				Blob blob = Hibernate.getLobCreator(this.sessionFactory.openSession()).createBlob(email.getBody().getBytes());
@@ -81,21 +70,5 @@ public class ScheduledTasks {
 			}
         	emailIter.remove();
         }
-    }
-    
-    private String wrapEmailTo(String to){
-    	return "<html><div><span>To:</span><span>"+to+"</span></div></html>";
-    }
-    
-    private String wrapEmailFrom(String from){
-    	return "<html><div><span>From:</span><span>"+from+"</span></div></html>";
-    }
-    
-    private String wrapEmailDate(Date date){
-    	return "<html><div><span>Received:</span><span>"+date.toString()+"</span></div></html>";
-    }
-    
-    private String wrapSubject(String subject){
-    	return "<html><p/><div><span>Subject:</span><span>"+subject+"</span></div></html>";
     }
 }
